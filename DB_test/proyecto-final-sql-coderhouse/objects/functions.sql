5af9d91ee34036313d696e311c65d66c4b1da8be
Henry Vazquez <henryjose2003@gmail.com>
2024-05-21 04:17:42 +0000

    creación de la "prueba_funcion" para probar la sintaxis en el contenedor

--- DB_test/proyecto-final-sql-coderhouse/objects/functions.sql
+++ DB_test/proyecto-final-sql-coderhouse/objects/functions.sql
@@ -2,7 +2,7 @@
 
 use kiosksdb;
 
--- FUNCIÓN 1 
+-- Función 1 
 /** Para la Tabla "Products" se crea la función para CALCULAR EL PRECIO DE VENTA de cada producto 
    eligiendo el porcentaje de ganancia que el vendedor desee (tomando en cuenta el precio de compra) **/
 /**delimiter //
@@ -17,7 +17,7 @@ create function `fn_calculate_sellsPricePerUnit` (_purchasesPricePerUnit float, 
     end //
 delimiter ;
 
--- FUNCIÓN 2
+-- Función 2
 delimiter //
 create function `fn_calculate_salaryPerDay` (_salaryPerHour float, _workedHoursPerDay int, _xDays int) 
 	returns float 
@@ -29,7 +29,8 @@ create function `fn_calculate_salaryPerDay` (_salaryPerHour float, _workedHoursP
     end //
 delimiter ;
 **/
--- FUNCIÓN 3 
+-- Función 3 
+/**
 delimiter //
 create function fn_calculate_billAmount (_idProduct int, _productQuantity int)
 returns float
@@ -48,4 +49,26 @@ begin
 		set _amount = _productQuantity * _sellsPrice;
 	return _amount;
 end //
+delimiter ;**/
+delimiter //
+CREATE FUNCTION prueba_funcion (_parametro1 INT, _parametro2 INT)
+RETURNS INT
+--read sql data 
+DETERMINISTIC
+BEGIN
+	--declare _sellsPrice float;
+	--declare _amount float;
+    DECLARE _result INT;
+
+		/**select
+			_sellsPricePerUnit into _sellsPrice
+		from 
+			kiosksdb.products as P
+		where 
+			_idProduct = P._idProduct;
+				
+		set _amount = _productQuantity * _sellsPrice;**/
+		SET _result = _parametro1 + _parametro2;
+	RETURN _result;
+END //
 delimiter ;
