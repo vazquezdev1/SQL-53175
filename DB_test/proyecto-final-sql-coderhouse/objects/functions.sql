7020f73c8f320b8cbdfe599680d98f80b55ea3cd
Henry Vazquez <henryjose2003@gmail.com>
2024-05-21 00:44:21 -0300

    Debugging archivo functions.sql

--- DB_test/proyecto-final-sql-coderhouse/objects/functions.sql
+++ DB_test/proyecto-final-sql-coderhouse/objects/functions.sql
@@ -1,11 +1,11 @@
 															-- FUNCIONES
 
 use kiosksdb;
-/**
+
 -- FUNCIÓN 1 
 /** Para la Tabla "Products" se crea la función para CALCULAR EL PRECIO DE VENTA de cada producto 
    eligiendo el porcentaje de ganancia que el vendedor desee (tomando en cuenta el precio de compra) **/
-delimiter //
+/**delimiter //
 create function `fn_calculate_sellsPricePerUnit` (_purchasesPricePerUnit float, _added_percentage float) 
 	returns float 
     deterministic
@@ -27,8 +27,8 @@ create function `fn_calculate_salaryPerDay` (_salaryPerHour float, _workedHoursP
         set _salaryPerXDays = (_salaryPerHour * _workedHoursPerDay) * _xDays; -- Al final multiplicamos la cantidad de horas trabajadas en un día por la cantidad de días ingresada
 		return _salaryPerXDays;
     end //
-delimiter ;**/
-
+delimiter ;
+**/
 -- FUNCIÓN 3 
 delimiter //
 create function `fn_calculate_billAmount`(_idProduct int, _productQuantity int)
